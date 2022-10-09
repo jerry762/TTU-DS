f3f934a5c2352a4d0419e8ea4ca03e28f7e4d8c8
Liu kuan-Tsen <75406330+jerry762@users.noreply.github.com>
2022-10-09 22:54:08 +0800

    completed hw14

--- HW14/Ans/hw14_1.cpp
+++ HW14/Ans/hw14_1.cpp
@@ -5,134 +5,195 @@
 #include <fstream>
 #include <string>
 #include <queue>
+#include <stack>
 
-#include "BFS.h" 
+#include "BFS.h"
 
 using namespace std;
 
-void display(int& aVertex)
+void display(int &aVertex)
 {
 	cout << " v" << aVertex;
 }
 
-void input(string sMessage, int& inputValue)
+void input(string sMessage, int &inputValue)
 {
-	do {
+	do
+	{
 		cout << sMessage << endl;
 		cin >> inputValue;
-	}  while ( cin.fail() );
+	} while (cin.fail());
 
 	return;
 }
 
 void input(string sMessage, string &sInputString)
 {
-	do {
+	do
+	{
 		cout << sMessage << endl;
 		cin >> sInputString;
-	}  while ( cin.fail() );
+	} while (cin.fail());
 
 	return;
 }
 
-void showAdjMatrix( int** matrixGraph, int numVertices )
+void showAdjMatrix(int **matrixGraph, int numVertices)
 {
-	for ( int i = 0; i < numVertices; i++ ) {
-		for ( int j = 0; j < numVertices; j++ ) {
-			if ( matrixGraph[ i ][ j ] == INT_MAX )
+	for (int i = 0; i < numVertices; i++)
+	{
+		for (int j = 0; j < numVertices; j++)
+		{
+			if (matrixGraph[i][j] == INT_MAX)
 				cout << " - ";
 			else
-				cout << ' ' << matrixGraph[ i ][ j ];
+				cout << ' ' << matrixGraph[i][j];
 		}
 		cout << '\n';
 	}
 }
 
-void DFS_Recu( int** matrixGraph, int numVertices, int vertexNo, bool* visited, vector <int> &parents )
+void DFS_Recu(int **matrixGraph, int numVertices, int vertexNo, bool *visited, vector<int> &parents)
 {
-	visited[ vertexNo ] = true;
+	visited[vertexNo] = true;
 	cout << " v" << vertexNo;
 
-	for ( int j = 0; j < numVertices; j++ ) {
-		if ( matrixGraph[ vertexNo ][ j ] < INT_MAX )
-			if ( !visited[ j ] ) {
-				parents[ j ] = vertexNo;
-				DFS_Recu( matrixGraph, numVertices, j, visited, parents );
+	for (int j = 0; j < numVertices; j++)
+	{
+		if (matrixGraph[vertexNo][j] < INT_MAX)
+			if (!visited[j])
+			{
+				parents[j] = vertexNo;
+				DFS_Recu(matrixGraph, numVertices, j, visited, parents);
 			}
 	}
 }
 
 void DFS_Iter(int **matrixGraph, int numVertices, int vertexNo, bool *visited, vector<int> &parents) //* add your code here
 {
+	stack<int> stack;
 
-	cout << " not implemented yet!\n";
-	return;
+	stack.push(vertexNo);
 
+	visited[vertexNo] = true;
+	cout << " v" << vertexNo;
+
+	while (!stack.empty())
+	{
+		bool find = false;
+
+		for (size_t i = 0; i < numVertices; i++)
+		{
+			if (matrixGraph[vertexNo][i] < INT_MAX && !visited[i])
+			{
+				parents[i] = vertexNo;
+				vertexNo = i;
+				stack.push(vertexNo);
+				visited[vertexNo] = true;
+				cout << " v" << vertexNo;
+				find = true;
+				break;
+			}
+		}
+
+		if (!find)
+		{
+			stack.pop();
+			if (!stack.empty())
+				vertexNo = stack.top();
+		}
+	}
 }
 
-vector<int> doDFS( int** matrixGraph, int numVertices, int vertexNo )
+vector<int> doDFS(int **matrixGraph, int numVertices, int vertexNo)
 {
-	bool* visited = new bool[numVertices];
-	vector <int> parents(numVertices, -1);
+	bool *visited = new bool[numVertices];
+	vector<int> parents(numVertices, -1);
 
-	for ( int i = 0; i < numVertices; i++ )
-		visited[ i ]= false;
+	for (int i = 0; i < numVertices; i++)
+		visited[i] = false;
 
 	cout << "\nDFS:";
-	DFS_Recu( matrixGraph, numVertices, vertexNo, visited, parents );
-	
-	cout << "\nDFS:";
-	DFS_Iter( matrixGraph, numVertices, vertexNo, visited, parents );
+	DFS_Recu(matrixGraph, numVertices, vertexNo, visited, parents);
 
-	delete [] visited;
+	for (int i = 0; i < numVertices; i++)
+		visited[i] = false;
+
+	cout << "\nDFS:";
+	DFS_Iter(matrixGraph, numVertices, vertexNo, visited, parents);
+
+	delete[] visited;
 
 	return parents;
 }
 
-void doBFS( Graph& listGraph, int vertexNo )
+void doBFS(Graph &listGraph, int vertexNo)
 {
 	cout << "BFS:";
-	BFS myBFS( listGraph );
-	myBFS.startSearch( vertexNo, display );
+	BFS myBFS(listGraph);
+	myBFS.startSearch(vertexNo, display);
 	cout << '\n';
 }
 
 vector<int> doBFS(int **matrixGraph, int numVertices, int vertexNo) //* add your code here
 {
-	vector <int> parents(numVertices, -1);
-	vector <bool> visited(numVertices, false);
+	vector<int> parents(numVertices, -1);
+	vector<bool> visited(numVertices, false);
+	queue<int> queue;
 
 	cout << "BFS: ";
-	cout << "not implemented yet!\n";
-	
+
+	queue.push(vertexNo);
+	visited[vertexNo] = true;
+
+	while (!queue.empty())
+	{
+		vertexNo = queue.front();
+		cout << " v" << vertexNo;
+		queue.pop();
+
+		for (size_t i = 0; i < numVertices; i++)
+		{
+			if (matrixGraph[vertexNo][i] < INT_MAX && !visited[i])
+			{
+				parents[i] = vertexNo;
+				queue.push(i);
+				visited[i] = true;
+			}
+		}
+	}
+
 	return parents;
 }
 
 bool checkCycle(int parent[], int u, int v) //* add your code here
 {
-
-
-
-
-	return false;
-
+	if (parent[v] == -1)
+	{
+		parent[v] = u;
+		return false;
+	}
+	return true;
 }
 
-bool doCheckCycles( int** matrixGraph, int numVertices )
+bool doCheckCycles(int **matrixGraph, int numVertices)
 {
 	int *parent = new int[numVertices];
-	
-	for ( int i = 0; i < numVertices; i++ ) 
-			parent[ i ] = -1;
 
-	for ( int u = 0; u < numVertices; u ++ ) {
-		for ( int v = u + 1; v < numVertices; v ++ ) {
-			if ( matrixGraph[ u ] [  v  ] < INT_MAX ) {
+	for (int i = 0; i < numVertices; i++)
+		parent[i] = -1;
+
+	for (int u = 0; u < numVertices; u++)
+	{
+		for (int v = u + 1; v < numVertices; v++)
+		{
+			if (matrixGraph[u][v] < INT_MAX)
+			{
 				cout << "( " << u << ", " << v << " )";
-				
-				if ( checkCycle( parent, u, v ) )
+
+				if (checkCycle(parent, u, v))
 					cout << " <= cycle created!";
-				
+
 				cout << "\n";
 			}
 		}
@@ -141,39 +202,40 @@ bool doCheckCycles( int** matrixGraph, int numVertices )
 	return false;
 }
 
-void printPathRecu(int** matrixGraph, vector <int> &parents, int vertex)
+void printPathRecu(int **matrixGraph, vector<int> &parents, int vertex)
 {
-	if ( parents[ vertex ] != -1 && parents[ vertex ] != vertex ) {
-		printPathRecu( matrixGraph, parents, parents[ vertex ]);
-		cout << " -> v" << vertex <<  ":" << matrixGraph[ parents[ vertex ] ][ vertex ];
+	if (parents[vertex] != -1 && parents[vertex] != vertex)
+	{
+		printPathRecu(matrixGraph, parents, parents[vertex]);
+		cout << " -> v" << vertex << ":" << matrixGraph[parents[vertex]][vertex];
 	}
 	else
 		cout << 'v' << vertex;
 }
 
-void printPathIter(int** matrixGraph, vector <int> &parents, int vertex)
+void printPathIter(int **matrixGraph, vector<int> &parents, int vertex)
 {
-	cout << 'v' << vertex ;
+	cout << 'v' << vertex;
 }
 
-void doFindPaths( int** matrixGraph, int numVertices, int vertexStartNo, int vertexEndNo )
+void doFindPaths(int **matrixGraph, int numVertices, int vertexStartNo, int vertexEndNo)
 {
 	vector<int> parents;
 
-	parents = doBFS( matrixGraph, numVertices, vertexStartNo );
-	
+	parents = doBFS(matrixGraph, numVertices, vertexStartNo);
+
 	cout << "\nPath: ";
-	printPathRecu( matrixGraph, parents, vertexEndNo );
+	printPathRecu(matrixGraph, parents, vertexEndNo);
 	cout << "\nPath: ";
-	printPathIter( matrixGraph, parents, vertexEndNo );
+	printPathIter(matrixGraph, parents, vertexEndNo);
 	cout << '\n';
 
-	parents = doDFS( matrixGraph, numVertices, vertexStartNo );
- 
+	parents = doDFS(matrixGraph, numVertices, vertexStartNo);
+
 	cout << "\nPath: ";
-	printPathRecu( matrixGraph, parents, vertexEndNo );
+	printPathRecu(matrixGraph, parents, vertexEndNo);
 	cout << "\nPath: ";
-	printPathIter( matrixGraph, parents, vertexEndNo );
+	printPathIter(matrixGraph, parents, vertexEndNo);
 
 	return;
 }
@@ -184,93 +246,108 @@ int main()
 	string sInput;
 	int numVertices, u, v, weight, vertexStartNo, vertexEndNo;
 
-	if ( !inFile ) {
+	if (!inFile)
+	{
 		cerr << "Error: Failed to open graph.txt" << endl;
 		return 1;
-	}else{
+	}
+	else
+	{
 		cout << "Success: Succeed to open graph.txt" << endl;
 	}
-		
-	if ( inFile.peek() == EOF ) {
+
+	if (inFile.peek() == EOF)
+	{
 		cerr << "Error: Empty input file! \n";
 		return 1;
 	}
 
 	inFile >> numVertices;
 
-	Graph listGraph( numVertices );
-	int **matrixGraph =  new int* [numVertices];
+	Graph listGraph(numVertices);
+	int **matrixGraph = new int *[numVertices];
 
-	for ( int i = 0; i < numVertices; i++ ) {
-		matrixGraph[ i ] = new int[numVertices];
-		
-		for ( int j = 0; j < numVertices; j++ )
-			matrixGraph[ i ][ j ] = INT_MAX;
+	for (int i = 0; i < numVertices; i++)
+	{
+		matrixGraph[i] = new int[numVertices];
 
-		matrixGraph[ i ][ i ] = 0;
+		for (int j = 0; j < numVertices; j++)
+			matrixGraph[i][j] = INT_MAX;
+
+		matrixGraph[i][i] = 0;
 	}
 
-	while ( inFile.peek() != EOF ) {
+	while (inFile.peek() != EOF)
+	{
 		inFile >> u >> v >> weight;
 
-		if ( inFile.fail() ) 
+		if (inFile.fail())
 			break;
 
-		if ( u < numVertices && v < numVertices ) {
-			Edge anEdge( u, v, weight );
-			listGraph.add( anEdge );
-			matrixGraph[ u ][ v ] = matrixGraph[ v ][ u ] = weight;
+		if (u < numVertices && v < numVertices)
+		{
+			Edge anEdge(u, v, weight);
+			listGraph.add(anEdge);
+			matrixGraph[u][v] = matrixGraph[v][u] = weight;
 		}
 	}
 
-	while (1) {
-		input( "\nEnter B to BFS, D to DFS, F to Find Paths, C to Check Cycles,\n      M to Find MST_K, S to Find Shortest Paths, or X to Exit", sInput );
-		
-		if ( sInput == "B" || sInput == "b" ) {
-			input( "Enter start vertex no: ",  vertexStartNo );
-			
-			if ( vertexStartNo < numVertices ) {
-				doBFS( listGraph, vertexStartNo );
-				doBFS( matrixGraph, numVertices, vertexStartNo );
+	while (1)
+	{
+		input("\nEnter B to BFS, D to DFS, F to Find Paths, C to Check Cycles,\n      M to Find MST_K, S to Find Shortest Paths, or X to Exit", sInput);
+
+		if (sInput == "B" || sInput == "b")
+		{
+			input("Enter start vertex no: ", vertexStartNo);
+
+			if (vertexStartNo < numVertices)
+			{
+				doBFS(listGraph, vertexStartNo);
+				doBFS(matrixGraph, numVertices, vertexStartNo);
 			}
-			else 
+			else
 				cerr << "Error: Vertex no out of range!  \n";
 		}
-		else  if ( sInput == "D" || sInput == "d" ) {
-			input( "Enter start vertex no: ",  vertexStartNo );
-			
-			if ( vertexStartNo < numVertices )
-				doDFS( matrixGraph, numVertices, vertexStartNo );
-			else 
+		else if (sInput == "D" || sInput == "d")
+		{
+			input("Enter start vertex no: ", vertexStartNo);
+
+			if (vertexStartNo < numVertices)
+				doDFS(matrixGraph, numVertices, vertexStartNo);
+			else
 				cerr << "Error: Vertex no out of range!  \n";
 		}
-		else  if ( sInput == "F" || sInput == "f" ) {
-			input( "Enter start vertex no: ",  vertexStartNo );
-			input( "Enter end vertex no: ",  vertexEndNo );
-			
-			if ( vertexStartNo < numVertices && vertexEndNo < numVertices ) {
-//				doBFS( matrixGraph, numVertices, vertexStartNo );
+		else if (sInput == "F" || sInput == "f")
+		{
+			input("Enter start vertex no: ", vertexStartNo);
+			input("Enter end vertex no: ", vertexEndNo);
+
+			if (vertexStartNo < numVertices && vertexEndNo < numVertices)
+			{
+				//				doBFS( matrixGraph, numVertices, vertexStartNo );
 				doFindPaths(matrixGraph, numVertices, vertexStartNo, vertexEndNo);
 				cout << '\n';
 			}
-			else 
+			else
 				cerr << "Error: Vertex out of range!  \n";
 		}
-		else  if ( sInput == "C" || sInput == "c" ) {
-			doCheckCycles( matrixGraph, numVertices );
+		else if (sInput == "C" || sInput == "c")
+		{
+			doCheckCycles(matrixGraph, numVertices);
 		}
-		else  if ( sInput == "X" || sInput == "x" ) {
-			if ( inFile.is_open() )
+		else if (sInput == "X" || sInput == "x")
+		{
+			if (inFile.is_open())
 				inFile.close();
 
-			for ( int i = 0; i < numVertices; i++ )
-				delete [] matrixGraph[i] ;
+			for (int i = 0; i < numVertices; i++)
+				delete[] matrixGraph[i];
 
 			delete matrixGraph;
 
 			return 0;
 		}
-		else 
+		else
 			cout << "Unknown command!\r" << endl;
 	}
 
