7f8164458ee25cc22835ad63baadc6cff77ba272
Liu kuan-Tsen <75406330+jerry762@users.noreply.github.com>
2022-12-11 02:48:18 +0800

    revise hw11 qs

--- HW11/Qs/hw11/BinarySearchTree.h
+++ HW11/Qs/hw11/BinarySearchTree.h
@@ -5,268 +5,274 @@
 
 /** Link-based implementation of the ADT binary search tree.
  @file BinarySearchTree.h */
- 
+
 #ifndef _BINARY_SEARCH_TREE
 #define _BINARY_SEARCH_TREE
 
 #include "BinaryTreeInterface.h"
 #include "BinaryNode.h"
 #include "BinaryNodeTree.h"
-//#include "NotFoundException.h"
+// #include "NotFoundException.h"
 #include "PrecondViolatedExcep.h"
 #include <queue>
 #include <stack>
 
-template<class ItemType>
+template <class ItemType>
 class BinarySearchTree : public BinaryNodeTree<ItemType>
 {
 private:
-   BinaryNode<ItemType>* rootPtr;
-   
-protected:
-   //------------------------------------------------------------
-   // Protected Utility Methods Section:
-   // Recursive helper methods for the public methods.
-   //------------------------------------------------------------
-   // Recursively finds where the given node should be placed and
-   // inserts it in a leaf at that point.
-   BinaryNode<ItemType>* insertInorder(BinaryNode<ItemType>* subTreePtr,
-                                       BinaryNode<ItemType>* newNode);
-   
-   // Removes the given target value from the tree while maintaining a
-   // binary search tree.
-   BinaryNode<ItemType>* removeValue(BinaryNode<ItemType>* subTreePtr,
-                                     const ItemType target,
-                                     bool& success);
-   
-   // Removes a given node from a tree while maintaining a
-   // binary search tree.
-   BinaryNode<ItemType>* removeNode(BinaryNode<ItemType>* nodePtr);
-   
-   // Removes the leftmost node in the left subtree of the node
-   // pointed to by nodePtr.
-   // Sets inorderSuccessor to the value in this node.
-   // Returns a pointer to the revised subtree.
-   BinaryNode<ItemType>* removeLeftmostNode(BinaryNode<ItemType>* nodePtr,
-                                            ItemType& inorderSuccessor, int& newWordCount);
-   
-   BinaryNode<ItemType>* removeRightmostNode(BinaryNode<ItemType>* nodePtr,
-                                            ItemType& inorderSuccessor, int& newWordCount);
-   
-   // Returns a pointer to the node containing the given value,
-   // or nullptr if not found.
-   BinaryNode<ItemType>* findNode(BinaryNode<ItemType>* treePtr,
-                                  const ItemType& target) const;
+	BinaryNode<ItemType> *rootPtr;
 
-   void inorder(void visit(BinaryNode<ItemType>&), BinaryNode<ItemType>* treePtr) const;
-   void levelorder(void visit(BinaryNode<ItemType>&), BinaryNode<ItemType>* treePtr) const;
-   
+protected:
+	//------------------------------------------------------------
+	// Protected Utility Methods Section:
+	// Recursive helper methods for the public methods.
+	//------------------------------------------------------------
+	// Recursively finds where the given node should be placed and
+	// inserts it in a leaf at that point.
+	BinaryNode<ItemType> *insertInorder(BinaryNode<ItemType> *subTreePtr,
+										BinaryNode<ItemType> *newNode);
+
+	// Removes the given target value from the tree while maintaining a
+	// binary search tree.
+	BinaryNode<ItemType> *removeValue(BinaryNode<ItemType> *subTreePtr,
+									  const ItemType target,
+									  bool &success);
+
+	// Removes a given node from a tree while maintaining a
+	// binary search tree.
+	BinaryNode<ItemType> *removeNode(BinaryNode<ItemType> *nodePtr);
+
+	// Removes the leftmost node in the left subtree of the node
+	// pointed to by nodePtr.
+	// Sets inorderSuccessor to the value in this node.
+	// Returns a pointer to the revised subtree.
+	BinaryNode<ItemType> *removeLeftmostNode(BinaryNode<ItemType> *nodePtr,
+											 ItemType &inorderSuccessor, int &newWordCount);
+
+	BinaryNode<ItemType> *removeRightmostNode(BinaryNode<ItemType> *nodePtr,
+											  ItemType &inorderSuccessor, int &newWordCount);
+
+	// Returns a pointer to the node containing the given value,
+	// or nullptr if not found.
+	BinaryNode<ItemType> *findNode(BinaryNode<ItemType> *treePtr,
+								   const ItemType &target) const;
+
+	void inorder(void visit(BinaryNode<ItemType> &), BinaryNode<ItemType> *treePtr) const;
+	void levelorder(void visit(BinaryNode<ItemType> &), BinaryNode<ItemType> *treePtr) const;
+
 public:
-   //------------------------------------------------------------
-   // Constructor and Destructor Section.
-   //------------------------------------------------------------
-   BinarySearchTree();
-   BinarySearchTree(const ItemType& rootItem);
-   BinarySearchTree(const BinarySearchTree<ItemType>& tree);
-   virtual ~BinarySearchTree();
-   
-   //------------------------------------------------------------
-   // Public Methods Section.
-   //------------------------------------------------------------
-   bool isEmpty() const;
-   //int getHeight() const;
-   //int getNumberOfNodes() const;
-   //ItemType getRootData() const throw(PrecondViolatedExcep);
-   //void setRootData(const ItemType& newData) const  throw(PrecondViolatedExcep);
-   bool add(const ItemType& newEntry);
-   bool remove(const ItemType& anEntry);
-//   void clear();
-//   ItemType getEntry(const ItemType& anEntry) const;
-//   throw(NotFoundException);
-//   bool contains(const ItemType& anEntry) const;
-   
-   //------------------------------------------------------------
-   // Public Traversals Section.
-   //------------------------------------------------------------
-   //void preorderTraverse(void visit(ItemType&)) const;
-   void inorderTraverse(void visit(BinaryNode<ItemType>&)) const;
-   //void postorderTraverse(void visit(ItemType&)) const;
-   void levelorderTraverse(void visit(BinaryNode<ItemType>&)) const;
-   
-   //------------------------------------------------------------
-   // Overloaded Operator Section.
-   //------------------------------------------------------------
-   BinarySearchTree<ItemType>& operator=(const BinarySearchTree<ItemType>& rightHandSide);   
+	//------------------------------------------------------------
+	// Constructor and Destructor Section.
+	//------------------------------------------------------------
+	BinarySearchTree();
+	BinarySearchTree(const ItemType &rootItem);
+	BinarySearchTree(const BinarySearchTree<ItemType> &tree);
+	virtual ~BinarySearchTree();
+
+	//------------------------------------------------------------
+	// Public Methods Section.
+	//------------------------------------------------------------
+	bool isEmpty() const;
+	// int getHeight() const;
+	// int getNumberOfNodes() const;
+	// ItemType getRootData() const throw(PrecondViolatedExcep);
+	// void setRootData(const ItemType& newData) const  throw(PrecondViolatedExcep);
+	bool add(const ItemType &newEntry);
+	bool remove(const ItemType &anEntry);
+	//   void clear();
+	//   ItemType getEntry(const ItemType& anEntry) const;
+	//   throw(NotFoundException);
+	//   bool contains(const ItemType& anEntry) const;
+
+	//------------------------------------------------------------
+	// Public Traversals Section.
+	//------------------------------------------------------------
+	// void preorderTraverse(void visit(ItemType&)) const;
+	void inorderTraverse(void visit(BinaryNode<ItemType> &)) const;
+	// void postorderTraverse(void visit(ItemType&)) const;
+	void levelorderTraverse(void visit(BinaryNode<ItemType> &)) const;
+
+	//------------------------------------------------------------
+	// Overloaded Operator Section.
+	//------------------------------------------------------------
+	BinarySearchTree<ItemType> &operator=(const BinarySearchTree<ItemType> &rightHandSide);
 }; // end BinarySearchTree
 
-template<class ItemType>
+template <class ItemType>
 BinarySearchTree<ItemType>::BinarySearchTree() : rootPtr(nullptr)
 {
-}  // end default constructor
+} // end default constructor
 
-template<class ItemType>
-BinarySearchTree<ItemType>::BinarySearchTree(const ItemType& rootItem)
+template <class ItemType>
+BinarySearchTree<ItemType>::BinarySearchTree(const ItemType &rootItem)
 {
-   rootPtr = new BinaryNode<ItemType>(rootItem, nullptr, nullptr);
-}  // end constructor
+	rootPtr = new BinaryNode<ItemType>(rootItem, nullptr, nullptr);
+} // end constructor
 
-template<class ItemType>
-BinarySearchTree<ItemType>::BinarySearchTree(const BinarySearchTree<ItemType>& treePtr)
+template <class ItemType>
+BinarySearchTree<ItemType>::BinarySearchTree(const BinarySearchTree<ItemType> &treePtr)
 {
-   rootPtr = copyTree(treePtr.rootPtr);
-}  // end copy constructor
+	rootPtr = copyTree(treePtr.rootPtr);
+} // end copy constructor
 
-template<class ItemType>
+template <class ItemType>
 BinarySearchTree<ItemType>::~BinarySearchTree()
 {
-   destroyTree( rootPtr );
-}  // end destructor
+	this->destroyTree(rootPtr);
+} // end destructor
 
 //////////////////////////////////////////////////////////////
 //      Protected Tree Traversal Sub-Section
 //////////////////////////////////////////////////////////////
 
-template<class ItemType>
-void BinarySearchTree<ItemType>::inorder(void visit(BinaryNode<ItemType>&), BinaryNode<ItemType>* treePtr) const
+template <class ItemType>
+void BinarySearchTree<ItemType>::inorder(void visit(BinaryNode<ItemType> &), BinaryNode<ItemType> *treePtr) const
 {
-   if (treePtr != nullptr)
-   {
-      inorder(visit, treePtr->getLeftChildPtr());
-      visit( *treePtr );
-      inorder(visit, treePtr->getRightChildPtr());
-   }  // end if
-}  // end inorder
+	if (treePtr != nullptr)
+	{
+		inorder(visit, treePtr->getLeftChildPtr());
+		visit(*treePtr);
+		inorder(visit, treePtr->getRightChildPtr());
+	} // end if
+} // end inorder
 
-
-
-template<class ItemType>
+template <class ItemType>
 bool BinarySearchTree<ItemType>::isEmpty() const
 {
-   return ( rootPtr == nullptr ) ;
-}  // end add
+	return (rootPtr == nullptr);
+} // end add
 
-
-template<class ItemType>
-void BinarySearchTree<ItemType>::inorderTraverse(void visit(BinaryNode<ItemType>&)) const
+template <class ItemType>
+void BinarySearchTree<ItemType>::inorderTraverse(void visit(BinaryNode<ItemType> &)) const
 {
-   inorder(visit, rootPtr);
-}  // end inorderTraverse
+	inorder(visit, rootPtr);
+} // end inorderTraverse
 
-template<class ItemType>
-void BinarySearchTree<ItemType>::levelorderTraverse(void visit(BinaryNode<ItemType>&)) const
+template <class ItemType>
+void BinarySearchTree<ItemType>::levelorderTraverse(void visit(BinaryNode<ItemType> &)) const
 {
-   levelorder(visit, rootPtr);
-}  // end inorderTraverse
+	levelorder(visit, rootPtr);
+} // end inorderTraverse
 
-template<class ItemType>
-void BinarySearchTree<ItemType>::levelorder(void visit(BinaryNode<ItemType>&), BinaryNode<ItemType>* treePtr) const
+template <class ItemType>
+void BinarySearchTree<ItemType>::levelorder(void visit(BinaryNode<ItemType> &), BinaryNode<ItemType> *treePtr) const
 {
 	// the data structures that may be useful
-	queue<BinaryNode<ItemType>*> nodeQueue;
-	stack<BinaryNode<ItemType>*> nodeStack;
+	queue<BinaryNode<ItemType> *> nodeQueue;
+	stack<BinaryNode<ItemType> *> nodeStack;
 	BinaryNode<ItemType> *tmpPtr = treePtr;
-
 
 	// the following is actually an interative inoder
 	// replace the code with yours
 
-
-	do {
-		while ( tmpPtr != NULL ) {
-			nodeStack.push( tmpPtr );
+	do
+	{
+		while (tmpPtr != NULL)
+		{
+			nodeStack.push(tmpPtr);
 			tmpPtr = tmpPtr->getLeftChildPtr();
 		}
 
-		if ( ! nodeStack.empty() ) {
+		if (!nodeStack.empty())
+		{
 			tmpPtr = nodeStack.top();
-			visit( *tmpPtr );
+			visit(*tmpPtr);
 			nodeStack.pop();
 			tmpPtr = tmpPtr->getRightChildPtr();
 		}
-	} while ( ! nodeStack.empty() || tmpPtr != NULL );
-}  // end levelorder
+	} while (!nodeStack.empty() || tmpPtr != NULL);
+} // end levelorder
 
-template<class ItemType>
-bool BinarySearchTree<ItemType>::add(const ItemType& newData)
+template <class ItemType>
+bool BinarySearchTree<ItemType>::add(const ItemType &newData)
 {
-   BinaryNode<ItemType>* newNodePtr = new BinaryNode<ItemType>(newData);
-   rootPtr = insertInorder(rootPtr, newNodePtr);
-   return true;
-}  // end add
+	BinaryNode<ItemType> *newNodePtr = new BinaryNode<ItemType>(newData);
+	rootPtr = insertInorder(rootPtr, newNodePtr);
+	return true;
+} // end add
 
-template<class ItemType>
-BinaryNode<ItemType>* BinarySearchTree<ItemType>::insertInorder(BinaryNode<ItemType>* subTreePtr,
-                                                            BinaryNode<ItemType>* newNodePtr)
+template <class ItemType>
+BinaryNode<ItemType> *BinarySearchTree<ItemType>::insertInorder(BinaryNode<ItemType> *subTreePtr,
+																BinaryNode<ItemType> *newNodePtr)
 {
 	if (subTreePtr == nullptr)
 		return newNodePtr;
 
-	BinaryNode<ItemType>* tempPtr;
+	BinaryNode<ItemType> *tempPtr;
 
-	if ( subTreePtr->getItem() > newNodePtr->getItem() ) {
-		tempPtr = insertInorder( subTreePtr->getLeftChildPtr(), newNodePtr );
-		subTreePtr->setLeftChildPtr( tempPtr );
+	if (subTreePtr->getItem() > newNodePtr->getItem())
+	{
+		tempPtr = insertInorder(subTreePtr->getLeftChildPtr(), newNodePtr);
+		subTreePtr->setLeftChildPtr(tempPtr);
 	}
-	else {
-		tempPtr = insertInorder( subTreePtr->getRightChildPtr(), newNodePtr );
-		subTreePtr->setRightChildPtr( tempPtr );
+	else
+	{
+		tempPtr = insertInorder(subTreePtr->getRightChildPtr(), newNodePtr);
+		subTreePtr->setRightChildPtr(tempPtr);
 	}
-	
+
 	return subTreePtr;
 }
 
-template<class ItemType>
-bool BinarySearchTree<ItemType>::remove(const ItemType& anEntry)
+template <class ItemType>
+bool BinarySearchTree<ItemType>::remove(const ItemType &anEntry)
 {
 	bool success = false;
-	rootPtr = removeValue( rootPtr, anEntry, success );
+	rootPtr = removeValue(rootPtr, anEntry, success);
 
 	return success;
 }
 
-template<class ItemType>
-BinaryNode<ItemType>* BinarySearchTree<ItemType>::removeValue(BinaryNode<ItemType>* subTreePtr,
-                                     const ItemType target,
-                                     bool& success)
+template <class ItemType>
+BinaryNode<ItemType> *BinarySearchTree<ItemType>::removeValue(BinaryNode<ItemType> *subTreePtr,
+															  const ItemType target,
+															  bool &success)
 {
-	if ( subTreePtr == nullptr ) {
+	if (subTreePtr == nullptr)
+	{
 		success = false;
 		return nullptr;
 	}
-	
-	if ( subTreePtr->getItem() == target ) {
-		subTreePtr = removeNode( subTreePtr );
+
+	if (subTreePtr->getItem() == target)
+	{
+		subTreePtr = removeNode(subTreePtr);
 		success = true;
 		return subTreePtr;
 	}
-	
-	BinaryNode<ItemType>* tempPtr;
 
-	if ( subTreePtr->getItem() > target ) {
-		tempPtr = removeValue( subTreePtr->getLeftChildPtr(), target, success );
-		subTreePtr->setLeftChildPtr( tempPtr );
+	BinaryNode<ItemType> *tempPtr;
+
+	if (subTreePtr->getItem() > target)
+	{
+		tempPtr = removeValue(subTreePtr->getLeftChildPtr(), target, success);
+		subTreePtr->setLeftChildPtr(tempPtr);
 		return subTreePtr;
 	}
-	else {
-		tempPtr = removeValue( subTreePtr->getRightChildPtr(), target, success );
-		subTreePtr->setRightChildPtr( tempPtr );
+	else
+	{
+		tempPtr = removeValue(subTreePtr->getRightChildPtr(), target, success);
+		subTreePtr->setRightChildPtr(tempPtr);
 		return subTreePtr;
 	}
 }
-   
-template<class ItemType>
-BinaryNode<ItemType>* BinarySearchTree<ItemType>::removeNode(BinaryNode<ItemType>* nodePtr)
+
+template <class ItemType>
+BinaryNode<ItemType> *BinarySearchTree<ItemType>::removeNode(BinaryNode<ItemType> *nodePtr)
 {
-	if ( nodePtr->isLeaf() ) {
+	if (nodePtr->isLeaf())
+	{
 		delete nodePtr;
 		nodePtr = nullptr;
 		return nodePtr;
-	} 
+	}
 
-	if ( nodePtr->getLeftChildPtr() == nullptr || nodePtr->getRightChildPtr() == nullptr ) {
-		BinaryNode<ItemType>* nodeToConnectPtr = nodePtr->getRightChildPtr();
+	if (nodePtr->getLeftChildPtr() == nullptr || nodePtr->getRightChildPtr() == nullptr)
+	{
+		BinaryNode<ItemType> *nodeToConnectPtr = nodePtr->getRightChildPtr();
 
-		if ( nodeToConnectPtr == nullptr )
+		if (nodeToConnectPtr == nullptr)
 			nodeToConnectPtr = nodePtr->getLeftChildPtr();
 
 		delete nodePtr;
@@ -274,41 +280,39 @@ BinaryNode<ItemType>* BinarySearchTree<ItemType>::removeNode(BinaryNode<ItemType
 		return nodeToConnectPtr;
 	}
 
-	BinaryNode<ItemType>* tempPtr;
+	BinaryNode<ItemType> *tempPtr;
 	ItemType newNodeValue;
 	int newWordCount;
 
-	tempPtr = removeLeftmostNode( nodePtr->getRightChildPtr(), newNodeValue, newWordCount );
-	nodePtr->setRightChildPtr( tempPtr );
-//	tempPtr = removeRightmostNode( nodePtr->getLeftChildPtr(), newNodeValue, newWordCount );
-//	nodePtr->setLeftChildPtr( tempPtr );
-	nodePtr->setItem( newNodeValue );
-	nodePtr->setCount( newWordCount );
+	tempPtr = removeLeftmostNode(nodePtr->getRightChildPtr(), newNodeValue, newWordCount);
+	nodePtr->setRightChildPtr(tempPtr);
+	//	tempPtr = removeRightmostNode( nodePtr->getLeftChildPtr(), newNodeValue, newWordCount );
+	//	nodePtr->setLeftChildPtr( tempPtr );
+	nodePtr->setItem(newNodeValue);
+	nodePtr->setCount(newWordCount);
 	return nodePtr;
 }
-   
-template<class ItemType>
-BinaryNode<ItemType>* BinarySearchTree<ItemType>::removeLeftmostNode(BinaryNode<ItemType>* nodePtr,
-                                            ItemType& inorderSuccessor, int& newWordCount)
+
+template <class ItemType>
+BinaryNode<ItemType> *BinarySearchTree<ItemType>::removeLeftmostNode(BinaryNode<ItemType> *nodePtr,
+																	 ItemType &inorderSuccessor, int &newWordCount)
 {
-	if ( nodePtr->getLeftChildPtr() == nullptr ) {
+	if (nodePtr->getLeftChildPtr() == nullptr)
+	{
 		inorderSuccessor = nodePtr->getItem();
 		newWordCount = nodePtr->getCount();
-		return removeNode( nodePtr );
-	} 
-	else 
-		return removeLeftmostNode( nodePtr->getLeftChildPtr(), inorderSuccessor, newWordCount );
+		return removeNode(nodePtr);
+	}
+	else
+		return removeLeftmostNode(nodePtr->getLeftChildPtr(), inorderSuccessor, newWordCount);
 }
 
-template<class ItemType>
-BinaryNode<ItemType>* BinarySearchTree<ItemType>::removeRightmostNode(BinaryNode<ItemType>* nodePtr,
-                                            ItemType& inorderPredecessor, int& newWordCount)
+template <class ItemType>
+BinaryNode<ItemType> *BinarySearchTree<ItemType>::removeRightmostNode(BinaryNode<ItemType> *nodePtr,
+																	  ItemType &inorderPredecessor, int &newWordCount)
 {
 
-
 	// add your code here
-
-
 }
 
 #endif
--- HW11/Qs/hw11/HW11.cpp
+++ HW11/Qs/hw11/HW11.cpp
@@ -1,7 +1,6 @@
-// HW11.cpp : ©w¸q¥D±±¥xÀ³¥Îµ{¦¡ªº¶i¤JÂI¡C
+// HW11.cpp : ï¿½wï¿½qï¿½Dï¿½ï¿½ï¿½xï¿½ï¿½ï¿½Îµ{ï¿½ï¿½ï¿½ï¿½ï¿½iï¿½Jï¿½Iï¿½C
 //
 
-#include "stdafx.h"
 #include <iostream>
 #include <string>
 #include "BinarySearchTree.h"
@@ -10,82 +9,95 @@ using namespace std;
 
 void input(string sMessage, string &sInputString)
 {
-	do {
+	do
+	{
 		cout << sMessage << endl;
 		cin >> sInputString;
-	} while ( cin.fail() );
+	} while (cin.fail());
 
 	return;
 }
 
-void display(BinaryNode<string>& aNode)
+void display(BinaryNode<string> &aNode)
 {
-	cout << aNode.getItem() << ":"<< aNode.getCount() << "  ";
+	cout << aNode.getItem() << ":" << aNode.getCount() << "  ";
 }
 
 int main()
 {
- 	string sInput, aString, aLine;
+	string sInput, aString, aLine;
 	BinarySearchTree<string> myBST;
 
-	while (1) {
-		input( "\nEnter command: A to Add, C to Count, R to Remove, or X to Exit", sInput );
-			
-		if ( sInput == "A"  || sInput == "a" ) {
-			input( "Enter a string: ",  aString );
-			myBST.add( aString );
-			cout << endl << "The inorder traversal after Add is " << endl;
-			myBST.inorderTraverse( display );
-			cout << endl << "The levelorder traversal after Add is " << endl;
-			myBST.levelorderTraverse( display );
-		} 
-		else if ( sInput == "R" || sInput == "r" ) {
-			input( "Enter a string: ",  aString );
-			try {
-				myBST.remove( aString );
+	while (1)
+	{
+		input("\nEnter command: A to Add, C to Count, R to Remove, or X to Exit", sInput);
 
-				if ( myBST.isEmpty() ) {
+		if (sInput == "A" || sInput == "a")
+		{
+			input("Enter a string: ", aString);
+			myBST.add(aString);
+			cout << endl
+				 << "The inorder traversal after Add is " << endl;
+			myBST.inorderTraverse(display);
+			cout << endl
+				 << "The levelorder traversal after Add is " << endl;
+			myBST.levelorderTraverse(display);
+		}
+		else if (sInput == "R" || sInput == "r")
+		{
+			input("Enter a string: ", aString);
+			try
+			{
+				myBST.remove(aString);
+
+				if (myBST.isEmpty())
+				{
 					cout << "The tree is now empty!" << endl;
 				}
-				else {
-					cout << endl << "The inoder traversal after Remove is " << endl;
-					myBST.inorderTraverse( display );
-					cout << endl << "The levelorder traversal after Remove is " << endl;
-					myBST.levelorderTraverse( display );
+				else
+				{
+					cout << endl
+						 << "The inoder traversal after Remove is " << endl;
+					myBST.inorderTraverse(display);
+					cout << endl
+						 << "The levelorder traversal after Remove is " << endl;
+					myBST.levelorderTraverse(display);
 				}
 			}
 			catch (...)
 			{
-				cout << "No item deleted!"  << endl;
+				cout << "No item deleted!" << endl;
 			}
 		}
-		else if ( sInput == "C"  || sInput == "c" ) {
+		else if (sInput == "C" || sInput == "c")
+		{
 			cin.ignore();
 			cout << "Enter a line of text: ";
-			getline( cin, aLine );
+			getline(cin, aLine);
 
 			string token, theDelimters = ".,!?;: ";
 			int end = 0;
-			int start = aLine.find_first_not_of( theDelimters, end );
+			int start = aLine.find_first_not_of(theDelimters, end);
 
-			while ( start != string::npos ) {
-				end = aLine.find_first_of( theDelimters, start );
-				token = aLine.substr( start, end - start );
-				myBST.add( token );
-				start = aLine.find_first_not_of( theDelimters, end );
+			while (start != string::npos)
+			{
+				end = aLine.find_first_of(theDelimters, start);
+				token = aLine.substr(start, end - start);
+				myBST.add(token);
+				start = aLine.find_first_not_of(theDelimters, end);
 			}
-			cout << endl << "The inoder traversal after Count is " << endl;
-			myBST.inorderTraverse( display );
-			cout << endl << "The levelorder traversal after Count is " << endl;
-			myBST.levelorderTraverse( display );
+			cout << endl
+				 << "The inoder traversal after Count is " << endl;
+			myBST.inorderTraverse(display);
+			cout << endl
+				 << "The levelorder traversal after Count is " << endl;
+			myBST.levelorderTraverse(display);
 		}
-		else  if ( sInput == "X" || sInput == "x" )
-			return 0;	
-		else 
+		else if (sInput == "X" || sInput == "x")
+			return 0;
+		else
 			cout << "Unknown command!\r" << endl;
 	}
 
 	return 0;
 }
-
-
